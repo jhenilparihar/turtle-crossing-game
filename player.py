f5b2c68b0b9acda963678c48a239a623b84cc1f9
Jhenil Parihar <jhenilparihar@gmail.com>
2021-10-04 11:34:42 +0530

    Update player.py

--- player.py
+++ player.py
@@ -7,9 +7,9 @@ FINISH_LINE_Y = 280
 class Player(Turtle):
     def __init__(self):
         super().__init__()
-        self.level_up()
+        self.reset_position()
 
-    def level_up(self):
+    def reset_position(self):
         self.shape("turtle")
         self.setheading(90)
         self.penup()
